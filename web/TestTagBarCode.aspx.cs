2eb36f4fb3294e5b5d364b1cdcc65a9744593a57
halfmoon2014 <mrzhangmh@gmail.com>
2019-05-04 17:24:04 +0800

    0504 add tagbarcode

---
+++ web/TestTagBarCode.aspx.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class TestTagBarCode : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        TTagBarCode.TagBarCode tag = new TTagBarCode.TagBarCode();
+        string t=tag.GetBarCode("12Q0704");
+    }
+}
